1bb1c3af986a0b556ed179d66cb63378f72f5f45
sayen akira <amardjebabla10@gmail.com>
2021-05-31 17:47:03 +0100

    Fixing Login and customazing Registration withing the same controller that includes both methodes

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class CustomAuthController extends Controller
+{
+
+    public function index()
+    {
+        return view('auth.login');
+    }  
+      
+
+    public function customLogin(Request $request)
+    {
+       
+        $request->validate([
+            'email' => ['required'],
+            'password' => ['required'],
+        ]);
+      
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+
+            return redirect()->intended('dashboard')
+                        ->withSuccess('Signed in');
+        }
+        
+  
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+      
+
+    public function customRegistration(Request $request)
+    {  
+     
+        $request->validate([
+            'matricule' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+      
+           
+        $data = $request->all();
+        
+        $check = $this->create($data);
+         
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }
+
+
+    protected function create(array $data)
+    {
+        return User::create([
+            'matricule' => $data['matricule'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+    
+
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+  
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+    
+
+    public function signOut() {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('login');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,40 +37,22 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="collapse navbar-collapse" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="collapse navbar-collapse" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register-user') }}">Register</a>
+                    </li>
+                    @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('signout') }}">Logout</a>
+                    </li>
+                    @endguest
+                </ul>
+            </div>
                 </div>
             </div>
         </nav>
