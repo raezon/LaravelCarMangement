a363b612fa8821c6e800bb276488a60fef5ccc34
sayen akira <amardjebabla10@gmail.com>
2021-06-01 10:55:43 +0100

    adding role_as to the migration of user

--- database/migrations/2021_05_27_135035_create_users_table.php
+++ database/migrations/2021_05_27_135035_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->String('password');
             $table->rememberToken();
+            $table->bigInteger('role_as');
             $table->timestamps();
         });
     }
